80c83bb9f101110873ea5650029e4d5a29e6cf71
nemeth.marcell <marcell.nemeth@linktoany.com>
2024-04-29 08:01:02 +0200

    LSRX-164

--- src/SentryErrorHandler.php
+++ src/SentryErrorHandler.php
@@ -8,6 +8,7 @@ use Sentry\State\HubInterface;
 use Sentry\State\Scope;
 use Szemul\ErrorHandler\Handler\ErrorHandlerInterface;
 use Szemul\LoggingErrorHandlingContext\ContextInterface;
+use Szemul\SentryErrorHandler\Factory\ClientHubFactory;
 use Szemul\SentryErrorHandler\Helper\SentryArrayHelper;
 use Throwable;
 
@@ -17,6 +18,7 @@ class SentryErrorHandler implements ErrorHandlerInterface
         protected HubInterface $sentryClientHub,
         protected ContextInterface $context,
         protected SentryArrayHelper $contextHelper,
+        protected ClientHubFactory $clientHubFactory,
         protected ?string $errorViewerBaseUrl = null,
     ) {
     }
@@ -56,7 +58,8 @@ class SentryErrorHandler implements ErrorHandlerInterface
 
     protected function sendExceptionToSentry(Throwable $exception, string $errorId): void
     {
-        $this->sentryClientHub->configureScope(
+        $sentryClientHub = $this->clientHubFactory->getSentryClientHub();
+        $sentryClientHub->configureScope(
             // Not using type hinting as Scope is final and doesn't implement an interface so it'd make this untestable
             function ($scope) use ($errorId) {
                 /** @var Scope $scope */
@@ -76,7 +79,7 @@ class SentryErrorHandler implements ErrorHandlerInterface
             },
         );
 
-        $this->sentryClientHub->captureException($exception);
+        $sentryClientHub->captureException($exception);
     }
 
     /**
